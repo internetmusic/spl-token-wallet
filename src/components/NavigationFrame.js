a742995763a81baec9332cbbf624ccdba882012d
Internet Music <thor@internetmusic.ltd.uk>
2020-12-17 01:25:31 +0100

    NIMWallet

--- src/components/NavigationFrame.js
+++ src/components/NavigationFrame.js
@@ -51,7 +51,7 @@ export default function NavigationFrame({ children }) {
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6" className={classes.title} component="h1">
-            CopyrightCoins Wallet
+            New Internet Media Wallet
           </Typography>
           <WalletSelector />
           <NetworkSelector />
